6c2923e9d6f9329c39f158068cabcbadb362d40a
Mar-fin <v.marvin.t@gmail.com>
2022-01-20 01:58:49 +0530

    JWT Implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 //User Models
@@ -40,9 +42,9 @@ router.post(
 
       //Get users gravatar
       const avatar = gravatar.url(email, {
-        s: 200,
-        r: pg,
-        d: mm,
+        s: '200',
+        r: 'pg',
+        d: 'mm',
       });
 
       user = new User({
@@ -60,11 +62,24 @@ router.post(
       await user.save();
 
       //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send('Server error');
     }
   }
 );
