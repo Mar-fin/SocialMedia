aefec1f3e2c030b0737242bd3cd0c565b2869d0f
Mar-fin <v.marvin.t@gmail.com>
2022-02-23 17:49:46 +0530

    Starting on Dashboard and Created Profile Component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
@@ -32,7 +38,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
 
   return (
-    <nav className='navbar bg-dark'>
+    <nav className=' bg-dark'>
       <h1>
         <a href='index.html'>
           <i className='fas fa-code'></i> DevConnector
